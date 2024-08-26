7945aedb412da33b88f9a38982013f0ab0532f17
rkhelgi <rkhelgi@unc.edu>
2024-08-26 10:46:30 -0400

    Finished Exercise 00!

---
+++ lessons/functions.py
@@ -0,0 +1,9 @@
+# Defining a function
+def sum(num1: int, num2: int) -> int:
+    """Return sum of 2 numbers."""
+    return num1 + num2
+
+
+# Call the function
+
+print(sum(num1=1, num2=10))  # <- 1 and 10 are arguments
